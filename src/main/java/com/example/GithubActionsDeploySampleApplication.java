7f561309b99144f4a0a1171b3bcbb5f84579607d
igamasayuki <igamasayuki@gmail.com>
2023-05-29 11:01:17 +0900

    first commit

---
+++ src/main/java/com/example/GithubActionsDeploySampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDeploySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsDeploySampleApplication.class, args);
+	}
+
+}
